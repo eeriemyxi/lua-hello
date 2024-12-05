d8f92b48720907ad3b475493cdc50124ac652b49
myxi <myxi@envs.net>
2024-12-05 13:46:10 +0530

    refactor: ditch lunacolors for color.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,12 @@
 argparse = require "ext.argparse"
-col      = require "ext.lunacolors"
+col      = require "ext.color"
 
 local parser = argparse("script", "An example script.")
 parser:option("-n --name", "A name.", "world")
 local args = parser:parse()
 
-print(col.brightWhite("Hello ") .. col.bold(col.green(args.name) .. col.reset() .. "!"))
+print(
+    col.fg.WHITE .. "Hello " .. 
+    col.bold .. col.fg.green .. args.name .. 
+    col.reset .. "!"
+)
